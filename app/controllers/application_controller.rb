5049ec408b7e69fb4b0f2ec75f9fd9ee3027d85d
Reto Inniger <reto.inniger@gmx.ch>
2017-04-03 17:57:02 +0200

    Iteration c5 fertig

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :date_loaded
+  	def date_loaded
+  		return Time.now
+  	end
+  before_filter :set_date
+		def set_date
+			@date_loaded = Time.now
+		end
+	before_action :set_date_loaded
+		private
+		def set_date_loaded
+			@date_loaded = Time.now
+		end
+
 end
