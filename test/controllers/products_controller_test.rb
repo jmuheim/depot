d588e2c280f4c9ee63cea3c6229d29b8dab50acb
Reto Inniger <reto.inniger@gmx.ch>
2017-04-12 12:34:08 +0200

    Iteration D5 fehler im Test
    Leider finde ich den Fehler nicht

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,10 +4,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
     @update = {
-      title: 'Lorem Ipsum',
+      title:       'Lorem Ipsum',
       description: 'Wibbles are fun!',
-      image_url: 'lorem.jpg',
-      price: 19.95
+      image_url:   'lorem.jpg',
+      price:       19.95
     }
   end
 
@@ -23,24 +23,25 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-    post products_url, params: { product: @update }
+      post products_url, params: { product: @update }
+    end
+
+    assert_redirected_to product_url(Product.last)
   end
 
-  assert_redirected_to product_url(Product.last)
+  test "should show product" do
+    get product_url(@product)
+    assert_response :success
+  end
 
+  test "should get edit" do
+    get edit_product_url(@product)
+    assert_response :success
   end
 
   test "should update product" do
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
-  end
-
-  test "can't delete product in cart" do
-    assert_difference('Product.count', 0) do
-      delete product_url(products(:two))
-    end
-
-    assert_redirected_to products_url
   end
 
   test "should destroy product" do
